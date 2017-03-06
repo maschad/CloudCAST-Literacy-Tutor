60fce8ec89c082c63ff5de2f74a8e1e5b68c6e7d
maschad <chad.nehemiah94@gmail.com>
2017-03-05 23:44:21 -0500

    first commit

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+import { AngularFireModule, AuthMethods } from 'angularfire2';
+
+
+const firebaseConfig = {
+   	apiKey: "AIzaSyA3Y24VTGHLp0DDRd92jBBRP4Tg_ay3g0s",
+    authDomain: "uwicloudcast.firebaseapp.com",
+    databaseURL: "https://uwicloudcast.firebaseio.com",
+    storageBucket: "uwicloudcast.appspot.com",
+};
+
+const firebaseAuthConfig = {
+  method: AuthMethods.Popup,
+  remember: 'default'
+};
+
+
+export const FirebaseModule = AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig);
