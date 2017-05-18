2b227b4c845c2b12b853c004b4563d16b1a28777
shamary <shamarywilliams@gmail.com>
2017-05-18 00:51:15 +0000

    more styles

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,8 @@ const ENV_DEVELOPMENT = NODE_ENV === 'development';
 const ENV_PRODUCTION = NODE_ENV === 'production';
 const ENV_TEST = NODE_ENV === 'test';
 
-const HOST = '0.0.0.0';
-const PORT = 3000;
+const HOST = process.env.IP;
+const PORT = process.env.PORT;
 
 
 //=========================================================
